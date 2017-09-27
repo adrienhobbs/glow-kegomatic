b90e6bbb1ab22ec95ec90866a496e2808e20f852
Adrien Hobbs <adrien.hobbs@gmail.com>
2017-09-27 14:18:32 -0400

    update

--- gpio-test.js
+++ gpio-test.js
@@ -1,6 +1,8 @@
 var Gpio = require('onoff').Gpio;
 var scale = new Gpio(27, 'in', 'both');
 
+console.log('listening on gpio: 27');
+
 scale.watch(function() {
   console.log('something happened');
 });
